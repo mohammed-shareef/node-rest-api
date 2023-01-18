3468ba9fc1c02e9e8084563e7af823733f435312
Mohammed Shareef <heh.nizam@outlook.com>
2023-01-18 10:21:53 +0000

    Adding 'if' statements for brevity and using return statements to prevent the error 'Cannot set headers after they are sent to the client'

--- routes/auth.js
+++ routes/auth.js
@@ -17,24 +17,41 @@ router.post("/register",async (req,res)=>{
      res.json(user);
    }
    catch(err){
-    console.log("Error when registering user"+err);
+    res.status(500).json(err);
    }
   });
 
   router.post("/login",async (req,res)=>{
     try{
       
+       if(!req.body.email) {
+         res.status(400).json("User name not provided"); 
+         return;
+       }
+
+       if(!req.body.password){
+         res.status(400).json("Password not provided");
+         return;
+       }
       const user = await User.findOne({"email": req.body.email});
-      !user && res.status(404).json("User not found"); 
+     
+       if(!user) {
+        res.status(404).json("User not found");
+        return;
+       } 
 
       const validPassword = await bcrypt.compare(req.body.password,user.password);
-      !validPassword && res.status(401).json("Wrong password"); 
-
+      
+      if(!validPassword) {
+        res.status(401).json("Wrong password"); 
+        return;
+      }
+      
       res.json(user);
 
     }
     catch(err){
-      console.log(err);
+      res.status(500).json(err);
     }
   });
 
