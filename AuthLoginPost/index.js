c0b663f31bb46116f62e18ff49ca6a4c6e8c766b
Nizam Ahmed <heh.nizam@outlook.com>
2023-01-28 12:57:11 +0000

    Added the login functionality

--- AuthLoginPost/index.js
+++ AuthLoginPost/index.js
@@ -2,18 +2,35 @@ const bcrypt = require("bcrypt");
 const User = require("../models/User");
 
 module.exports = async function (context, req) {
-    
-    const salt = await bcrypt.genSalt(10);
-    
-    context.log('JavaScript HTTP trigger function processed a request.');
-
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
-
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
+    try{
+      
+        if(!req.body.email) {
+          context.res = { body: "User name not provided"}; 
+          return;
+        }
+ 
+        if(!req.body.password){
+            context.res = { body: "Password not provided"};
+          return;
+        }
+       const user = await User.findOne({"email": req.body.email});
+      
+        if(!user) {
+            context.res = { body: "User not found"};
+         return;
+        } 
+ 
+       const validPassword = await bcrypt.compare(req.body.password,user.password);
+       
+       if(!validPassword) {
+        context.res = { body: "Wrong password" }; 
+         return;
+       }
+       
+       res.json(user);
+ 
+     }
+     catch(err){
+        context.res = { body : err};
+     }
 }
