e8a3a9216124142e850c8bec5306f365ab43f049
Nizam Ahmed <heh.nizam@outlook.com>
2023-01-28 11:58:23 +0000

    Initial version of a function to serve a login post api

---
+++ AuthLoginPost/index.js
@@ -0,0 +1,19 @@
+const bcrypt = require("bcrypt");
+const User = require("../models/User");
+
+module.exports = async function (context, req) {
+    
+    const salt = await bcrypt.genSalt(10);
+    
+    context.log('JavaScript HTTP trigger function processed a request.');
+
+    const name = (req.query.name || (req.body && req.body.name));
+    const responseMessage = name
+        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
+        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+
+    context.res = {
+        // status: 200, /* Defaults to 200 */
+        body: responseMessage
+    };
+}
