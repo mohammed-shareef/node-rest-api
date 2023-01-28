f195909ed30047d6840d079ee75524a50aa1be5c
Nizam Ahmed <heh.nizam@outlook.com>
2023-01-28 18:02:55 +0000

    Added status back to the json responses

--- AuthLoginPost/index.js
+++ AuthLoginPost/index.js
@@ -5,32 +5,54 @@ module.exports = async function (context, req) {
     try{
       
         if(!req.body.email) {
-          context.res = { body: "User name not provided"}; 
+          context.res = { 
+            status: 400,
+            body: "Email not provided"
+          }; 
+          return;
+        }
+
+        if(!req.body.userName) {
+          context.res = { 
+            status: 400,
+            body: "User name not provided"
+          }; 
           return;
         }
  
         if(!req.body.password){
-            context.res = { body: "Password not provided"};
+            context.res = {
+              status: 400,
+              body: "Password not provided"
+            };
           return;
         }
        const user = await User.findOne({"email": req.body.email});
       
         if(!user) {
-            context.res = { body: "User not found"};
+            context.res = {
+               status: 404,
+               body: "User not found"
+              };
          return;
         } 
  
        const validPassword = await bcrypt.compare(req.body.password,user.password);
        
        if(!validPassword) {
-        context.res = { body: "Wrong password" }; 
+        context.res = { 
+          status: 401,
+          body: "Wrong password" }; 
          return;
        }
        
-       context.res = {body : user};
+       context.res = {
+        body : user};
  
      }
      catch(err){
-        context.res = { body : err};
+        context.res = {
+          status: 500,
+           body : 'Unable to login - '+err};
      }
 }
