ca93ed740cf52e092d41a897272b5ca888d6ce51
Nizam Ahmed <heh.nizam@outlook.com>
2023-02-02 10:51:26 +0000

    Added an end point to get all the posts for a users timeline

---
+++ PostTimelineGet/index.js
@@ -0,0 +1,23 @@
+
+const User = require("../models/User");
+const Post = require("../models/Post");
+
+module.exports = async function (context, req) {
+    try{
+        const currentUser = await User.findById(req.body.userId);
+        const userPosts = await Post.find({userId : currentUser._id});
+        const friendPosts = await Promise.all(
+         currentUser.following.map(friendId =>{
+           return Post.find({userId : friendId});
+         }));
+         context.res = {
+            body : userPosts.concat(...friendPosts)
+         };
+     }
+     catch(err){
+       context.res = {
+          status : 500,
+          body : 'Could not show timeline - '+err
+       }
+     }
+}
