c91c475786446cfa8b5bb8da0950a210e905ce8a
Nizam Ahmed <heh.nizam@outlook.com>
2023-02-02 12:25:23 +0000

    Updated the logic to return a post by id

--- PostUpdatePut/index.js
+++ PostUpdatePut/index.js
@@ -13,20 +13,11 @@ module.exports = async function (context, req) {
             };
             return;
         }
+
+        context.res = {
+          body : post
+      };
     
-        if(req.context.body.userId !== post.userId){
-            context.res = {
-                status : 403,
-                body : "you can only update your own posts"
-            };
-            return;
-        }
-        else{
-          await post.updateOne({$set : req.context.body});
-          context.res = {
-            body : "updated post successfully"
-          };
-        }
       }
       catch(err){
         context.res = {
